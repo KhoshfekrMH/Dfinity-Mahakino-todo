fdf97b35bc027811bcffd402467a603f2840da4b
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2023-04-10 13:38:28 +0330

    first init

---
+++ src/dfinityMahakinoTodo_frontend/src/index.jsx
@@ -0,0 +1,12 @@
+import { dfinityMahakinoTodo_backend } from '../../declarations/dfinityMahakinoTodo_backend';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const init = async () => {
+  const root = ReactDOM.createRoot ( document.getElementById ( 'root' ) );
+  root.render (
+    <div>HelloWorld</div>
+  );
+};
+
+init();
