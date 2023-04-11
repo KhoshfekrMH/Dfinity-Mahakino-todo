046e86743b1b52ae2c8d0e192b81cbcbf8463c28
KhoshfekrMH <Khoshfekr2001@protonmail.com>
2023-04-11 12:05:07 +0330

    Note Area added to show created note under create area

--- src/dfinityMahakinoTodo_frontend/src/components/App.jsx
+++ src/dfinityMahakinoTodo_frontend/src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import {Footer} from "./Footer";
 import {Header} from "./Header";
 import {CreateArea} from "./CreateArea";
+import {Note} from "./Note";
 
 export const App = () => {
     return (
         <div>
             <Header />
             <CreateArea />
+            <Note />
             <Footer />
         </div>
     );
